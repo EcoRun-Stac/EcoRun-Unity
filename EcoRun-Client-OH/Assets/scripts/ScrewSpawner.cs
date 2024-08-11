abf46944f01f83299e64b5ea99ef4c5f84e423dc
오지은 <w2317@e-mirim.hs.kr>
2024-08-11 23:06:19 +0900

    [OH] 적 생성 추가

--- EcoRun-Client-OH/Assets/scripts/ScrewSpawner.cs
+++ EcoRun-Client-OH/Assets/scripts/ScrewSpawner.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ScrewSpawner : MonoBehaviour
 {
-    public GameObject screwPrefab;
+    public GameObject CoinPrefab;
+    public GameObject EnemyPrefab;
     public float timeBetSpawnMin = 0f;
     public float timeBetSpawnMax = 10f;
     private float timeBetSpawn; //다음배치까지의 시간 간격
     private float xPos = 20f; // x 소환 위치
     private float lastSpawnTime;
+    public int time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,7 @@ public class ScrewSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time++;
         if (Time.time >= lastSpawnTime + timeBetSpawn)
         {
             //기록된 마지막 배치 시점을 현재 시점으로 갱신
@@ -32,6 +35,13 @@ public class ScrewSpawner : MonoBehaviour
     }
     public void newObject()
     {
-        Instantiate(screwPrefab, new Vector3(14.23f, -1.415f, 0), Quaternion.identity);
+        if (time%10 == 3)
+        {
+            Instantiate(EnemyPrefab, new Vector3(14.23f, -1.415f, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(CoinPrefab, new Vector3(14.23f, -1.415f, 0), Quaternion.identity);
+        }
     }
 }
