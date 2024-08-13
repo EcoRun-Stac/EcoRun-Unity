ba77832c4f4dabc46429c1fee4165051152d6ddf
오지은 <w2317@e-mirim.hs.kr>
2024-08-13 21:41:25 +0900

    [OH] 스코어 표현 수정

--- EcoRun-Client-OH/Assets/scripts/ScoreManager.cs
+++ EcoRun-Client-OH/Assets/scripts/ScoreManager.cs
@@ -2,41 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-    private int score;
+    private int item;
     public int heartCount = 0;
     public HeartManager heartManager;
 
     void Start()
     {
-        score = 0;
-        UpdateScore(); // 초기 점수 설정
+        item = 0;
     }
-
-    public void IncreaseScore()
-    {
-        score += 1;
-        UpdateScore();
-    }
-
     public void DecreaseHeart()
     {
-        heartManager.DestroyHeart(heartCount);
-        heartCount++;
+
+        if (heartCount >= 0 && heartCount < 5)
+        {
+            heartManager.DestroyHeart(heartCount);
+            heartCount++;
+        }
+        
     }
 
-    void UpdateScore()
+    public void IncreaseItem()
     {
-        if (scoreText != null)
-        {
-            scoreText.text = string.Format("X {0:D3}", score);
-        }
-        else
-        {
-            Debug.LogError("ScoreText가 null입니다. UI를 확인하세요.");
-        }
+        item++;
+        scoreText.text = string.Format("X {0:D3}", item);
+        Debug.Log(item);
     }
+
+    
 }
